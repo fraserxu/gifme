670c2e2f75092ec3fcc1215bcdc2ce2b3dbeeb46
fraserxu <xvfeng123@gmail.com>
2014-01-11 20:25:26 +0800

    add task.js and dependencies
    Signed-off-by: fraserxu <xvfeng123@gmail.com>

---
+++ task.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var vinyl = require('vinyl-fs');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var catw = require('catw');
+
+var cmd = process.argv[2];
+if (cmd === 'build') build({ watch: false })
+else if (cmd=== 'watch') build({ watch: true })
+else usage(1)
+
+function build (opts) {
+  var js = opts.watch ? watchify : browserify;
+  js('./browser/main.js').bundle()
+    .pipe(fs.createWriteStream('./static/bundle.js'))
+  ;
+
+  var assets = vinyl.src('./browser/assets/**/*');
+  assets.pipe(vinyl.dest('./static/assets'));
+
+  var index = catw('./browser/index.html', { watch: opts.watch });
+  index.on('stream', function (stream) {
+    stream.pipe(fs.createWriteStream('./static/index.html'))
+  });
+
+  var css = catw('./browser/*.css', { watch: opts.watch });
+  css.on('stream', function (stream) {
+    stream.pipe(fs.createWriteStream('./static/bundle.css'))
+  });
+}
+
+function usage (code) {
+  console.error('usage: ./task.js { build | watch }');
+  if (code) process.exit(code)
+}
